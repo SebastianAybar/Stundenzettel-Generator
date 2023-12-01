de92a93f7cfb52b5e223d34c7c6488ca5a620bcd
Sebastian Aybar <38281694+SebiAybar@users.noreply.github.com>
2023-12-01 23:11:23 +0100

    choose file method

--- Stundenzettel/src/main/java/com/example/stundenzettel/ApplicationStart.java
+++ Stundenzettel/src/main/java/com/example/stundenzettel/ApplicationStart.java
@@ -7,10 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class main extends Application {
+public class ApplicationStart extends Application {
+    private int windowWidth = 710;
+    private int windowHeight = 460;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ApplicationStart.class.getResource("stundenzettel.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         //scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         String css = this.getClass().getResource("styles.css").toExternalForm();
@@ -18,8 +21,8 @@ public class main extends Application {
         stage.setTitle("Stundenzettel Generator");
         stage.setScene(scene);
         stage.show();
-        stage.setMinWidth(685);
-        stage.setMinHeight(415);
+        stage.setMinWidth(windowWidth);
+        stage.setMinHeight(windowHeight);
     }
 
     public static void main(String[] args) {
--- Stundenzettel/src/main/java/com/example/stundenzettel/StundenzettelController.java
+++ Stundenzettel/src/main/java/com/example/stundenzettel/StundenzettelController.java
@@ -4,13 +4,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Line;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-public class Controller {
+import java.io.File;
+
+public class StundenzettelController {
     @FXML private AnchorPane anchorPane;
     @FXML private Button excelList;
     @FXML private Button einzelerstellung;
     @FXML private Button calculate;
-    @FXML private Button inputPathSearch;
+    @FXML private Button btnChooseInputFile;
     @FXML private Button outputPathSearch;
     @FXML private Label inputFile;
     @FXML private Label outputTextField;
@@ -20,4 +24,15 @@ public class Controller {
     @FXML private CheckBox replaceFile;
     @FXML private Separator separator1;
     @FXML private Separator separator2;
+
+    @FXML
+    protected void chooseFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Datei auswählen");
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            inputPathTextField.setText(selectedFile.getAbsolutePath());
+        }
+    }
 }
